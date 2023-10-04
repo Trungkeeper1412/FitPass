887bbd1f15e75a614bdc203c8c495c6716ee04d9
letrungduc2832 <letrungduc2832@gmail.com>
2023-10-04 17:21:05 +0700

    trungduc_change log : _add query to IRepositoryQuery : GET_USER_DETAIL_BY_USER_ID, UPDATE_USER_DETAIL
    _ add methods and implentions : findByUserId, updateUserDetail
    
    _ add new java class UserProfileController

--- fitpass-core/src/main/java/com/ks/fitpass/core/repository/IRepositoryQuery.java
+++ fitpass-core/src/main/java/com/ks/fitpass/core/repository/IRepositoryQuery.java
@@ -28,4 +28,32 @@ public interface IRepositoryQuery {
     String GET_KBN_BY_NAME = """
             SELECT * FROM mst_kbn WHERE mst_kbn_name = ?
         """;
+
+    String GET_USER_DETAIL_BY_USER_ID = """
+            SELECT
+                T1.user_id,
+                T1.first_name,
+                T1.last_name,
+                T1.email,
+                T1.phone_number,
+                T1.address,
+                T1.gender,
+                T2.role_id,
+                T2.role_name
+            FROM user_detail T1
+            INNER JOIN role T2
+                ON T1.role_id = T2.role_id
+            WHERE T1.user_id = ?
+        """;
+
+    String UPDATE_USER_DETAIL = """
+            UPDATE user_detail
+            SET first_name = ?,
+                last_name = ?,
+                email = ?,
+                phone_number = ?,
+                address = ?,
+                gender = ?
+            WHERE user_id = ?
+        """;
 }
