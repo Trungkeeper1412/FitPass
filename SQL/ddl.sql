5c226adda61f10e6de33a703fa0e97d075f295b4
trungkeeper <trung14112002wweliker@gmail.com>
2023-10-11 01:57:08 +0700

    TrungNV: fix dml and ddl

--- SQL/ddl.sql
+++ SQL/ddl.sql
@@ -1,39 +1,20 @@
--- Admin creation
-INSERT INTO user_detail (user_detail_id, first_name, last_name, email, phone_number, address,
-                         date_of_birth,
-                         gender)
-VALUES (1, 'John', 'Doe', 'johndoe@example.com', '1234567890', '123 Main St, City, Country', '1990-01-01', 'Male');
-
-INSERT INTO user (user_id, user_account, user_password, user_create_time, user_deleted,user_detail_id)
-VALUES (1, 'admin', '$2a$12$RtKhDBN9Ba8UlVcAulEenOsxqHK5ZNQ1Lj62508aqPwg9Jbbv6/c2', '1655989807', 0, 1);
-
--- Gym Owner creation
-INSERT INTO user_detail (user_detail_id, first_name, last_name, email, phone_number, address,
-                         date_of_birth,
-                         gender)
-VALUES (2, 'Jane', 'Doe', 'janedoe@example.com', '1234567890', '123 Main St, City, Country', '1990-01-01', 'Female');
-
-INSERT INTO user (user_id, user_account, user_password, user_create_time, user_deleted,user_detail_id)
-VALUES (2, 'gymowner', '$2a$12$RtKhDBN9Ba8UlVcAulEenOsxqHK5ZNQ1Lj62508aqPwg9Jbbv6/c2', '1655989807', 0, 2);
-
-
-INSERT INTO role (role_id, role_name) VALUES (1, 'ROLE_ADMIN');
-INSERT INTO role (role_id, role_name) VALUES (2, 'ROLE_MANAGER');
-INSERT INTO role (role_id, role_name) VALUES (3, 'ROLE_EMPLOYEE');
-INSERT INTO role (role_id, role_name) VALUES (4, 'ROLE_MEMBER');
-
-
-INSERT INTO user_role (user_role_id, user_id, role_id) VALUES (1, 1, 1);
-INSERT INTO user_role (user_role_id, user_id, role_id) VALUES (2, 2, 2);
-
--- Gym Department Creation
--- Create Department Status
-INSERT INTO mst_kbn (mst_kbn_name, mst_kbn_key, mst_kbn_value) VALUES ('DEPARTMENT_STATUS', 1, 'Hoạt Động');
-INSERT INTO mst_kbn (mst_kbn_name, mst_kbn_key, mst_kbn_value) VALUES ('DEPARTMENT_STATUS', 2, 'Không Hoạt Động');
-
-
-
-
+-- Drop queries to remove existing tables (if they exist)
+DROP TABLE IF EXISTS user_feedback;
+DROP TABLE IF EXISTS item_inventory;
+DROP TABLE IF EXISTS order_plan_detail;
+DROP TABLE IF EXISTS `order`;
+DROP TABLE IF EXISTS `transaction`;
+DROP TABLE IF EXISTS transfer;
+DROP TABLE IF EXISTS gymer_booking;
+DROP TABLE IF EXISTS shift;
+DROP TABLE IF EXISTS gym_plan;
+DROP TABLE IF EXISTS mst_kbn;
+DROP TABLE IF EXISTS gym_department;
+DROP TABLE IF EXISTS user_role;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS wallet;
+DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS user_detail;
 
 CREATE TABLE IF NOT EXISTS user_detail (
                                            user_detail_id       INT AUTO_INCREMENT PRIMARY KEY,
@@ -94,7 +75,7 @@ CREATE TABLE IF NOT EXISTS gym_department (
     description       TEXT NOT NULL,
     latitude 			DECIMAL(10,8) NOT NULL,
     longitude 		DECIMAL(11,8) NOT NULL,
-    rating            DECIMAL(10, 2) NOT NULL,
+    rating            DECIMAL(10, 2) DEFAULT 0,
     FOREIGN KEY (user_id) REFERENCES `user`(user_id)
     );
 
