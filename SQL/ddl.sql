47c21a66392bafb79bdefde6565fd4640c15b708
trungkeeper <trung14112002wweliker@gmail.com>
2023-10-15 23:03:42 +0700

    TrungNV: update dml and ddl

--- SQL/ddl.sql
+++ SQL/ddl.sql
@@ -9,6 +9,10 @@ DROP TABLE IF EXISTS gymer_booking;
 DROP TABLE IF EXISTS shift;
 DROP TABLE IF EXISTS gym_plan;
 DROP TABLE IF EXISTS mst_kbn;
+DROP TABLE IF EXISTS gym_department_features;
+DROP TABLE IF EXISTS gym_department_services;
+DROP TABLE IF EXISTS gym_department_schedule;
+DROP TABLE IF EXISTS gym_department_albums;
 DROP TABLE IF EXISTS gym_department;
 DROP TABLE IF EXISTS user_role;
 DROP TABLE IF EXISTS role;
@@ -19,36 +23,36 @@ DROP TABLE IF EXISTS user_detail;
 CREATE TABLE IF NOT EXISTS user_detail (
                                            user_detail_id       INT AUTO_INCREMENT PRIMARY KEY,
                                            first_name    VARCHAR(50) NOT NULL,
-    last_name     VARCHAR(50) NOT NULL,
-    email         VARCHAR(100) NOT NULL,
-    phone_number  VARCHAR(20) NOT NULL,
-    address       VARCHAR(255) NOT NULL,
-    date_of_birth DATE NOT NULL,
-    gender        VARCHAR(10) NOT NULL
+                                           last_name     VARCHAR(50) NOT NULL,
+                                           email         VARCHAR(100) NOT NULL,
+                                           phone_number  VARCHAR(20) NOT NULL,
+                                           address       VARCHAR(255) NOT NULL,
+                                           date_of_birth DATE NOT NULL,
+                                           gender        VARCHAR(10) NOT NULL
 
-    );
+);
 -- User table to store user information
 CREATE TABLE IF NOT EXISTS `user` (
                                       user_id          INT AUTO_INCREMENT PRIMARY KEY,
                                       user_account     VARCHAR(250) NOT NULL,
-    user_password    VARCHAR(100) NOT NULL,
-    user_detail_id        INT NOT NULL,
-    user_create_time VARCHAR(20) NOT NULL,
-    user_deleted     TINYINT NOT NULL,
-    FOREIGN KEY (user_detail_id) REFERENCES user_detail(user_detail_id)
-    );
+                                      user_password    VARCHAR(100) NOT NULL,
+                                      user_detail_id        INT NOT NULL,
+                                      user_create_time VARCHAR(20) NOT NULL,
+                                      user_deleted     TINYINT NOT NULL,
+                                      FOREIGN KEY (user_detail_id) REFERENCES user_detail(user_detail_id)
+);
 -- User Wallet table to store user wallet information
 CREATE TABLE IF NOT EXISTS wallet (
                                       wallet_id     INT AUTO_INCREMENT PRIMARY KEY,
                                       user_id       INT NOT NULL,
                                       balance       DECIMAL(10, 2) DEFAULT 0.0,
-    FOREIGN KEY (user_id) REFERENCES user(user_id)
-    );
+                                      FOREIGN KEY (user_id) REFERENCES user(user_id)
+);
 -- Role table to store user roles
 CREATE TABLE IF NOT EXISTS role (
                                     role_id   INT AUTO_INCREMENT PRIMARY KEY,
                                     role_name VARCHAR(20) NOT NULL
-    );
+);
 
 -- User Role table to store the relationship between users and roles
 CREATE TABLE IF NOT EXISTS user_role (
@@ -56,54 +60,82 @@ CREATE TABLE IF NOT EXISTS user_role (
                                          user_id      INT NOT NULL,
                                          role_id      INT NOT NULL,
                                          FOREIGN KEY (user_id) REFERENCES `user` (user_id),
-    FOREIGN KEY (role_id) REFERENCES role (role_id)
-    );
-
-
+                                         FOREIGN KEY (role_id) REFERENCES role (role_id)
+);
 
 -- Gym Department table to store gym department information
 CREATE TABLE IF NOT EXISTS gym_department (
-                                              gym_department_id INT AUTO_INCREMENT PRIMARY KEY,
-                                              gym_department_status_key INT NOT NULL,
-                                              user_id           INT NOT NULL,
-                                              name              VARCHAR(255) NOT NULL,
-    address           VARCHAR(255) NOT NULL,
-    contact_number    VARCHAR(20) NOT NULL,
-    logo_url          VARCHAR(255) NOT NULL,
-    opening_hours     VARCHAR(255) NOT NULL,
-    image_url         VARCHAR(255) NOT NULL,
-    description       TEXT NOT NULL,
-    latitude 			DECIMAL(10,8) NOT NULL,
-    longitude 		DECIMAL(11,8) NOT NULL,
-    rating            DECIMAL(10, 2) DEFAULT 0,
-    FOREIGN KEY (user_id) REFERENCES `user`(user_id)
-    );
+                                              gym_department_id           INT AUTO_INCREMENT PRIMARY KEY,
+                                              gym_department_status_key   INT NOT NULL,
+                                              user_id                     INT NOT NULL,
+                                              name                        VARCHAR(255) NOT NULL,
+                                              address                     VARCHAR(255) NOT NULL,
+                                              contact_number              VARCHAR(20) NOT NULL,
+                                              logo_url                    VARCHAR(255) NOT NULL,
+                                              wallpaper_url               VARCHAR(255) NOT NULL,
+                                              description                 text,
+                                              latitude 		            DECIMAL(10,8) NOT NULL,
+                                              longitude 		            DECIMAL(11,8) NOT NULL,
+                                              rating                      DECIMAL(10, 2) DEFAULT 0,
+                                              capacity                    INT NOT NULL,
+                                              area                        DECIMAL(10, 2),
+                                              FOREIGN KEY (user_id) REFERENCES `user`(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS gym_department_albums (
+                                                     id INT PRIMARY KEY AUTO_INCREMENT,
+                                                     gym_department_id INT NOT NULL,
+                                                     photo_url VARCHAR(255) NOT NULL,
+                                                     description VARCHAR(255),
+                                                     FOREIGN KEY(gym_department_id) REFERENCES gym_department(gym_department_id)
+);
+
+CREATE TABLE IF NOT EXISTS gym_department_schedule (
+                                                       id INT AUTO_INCREMENT PRIMARY KEY,
+                                                       gym_department_id INT NOT NULL,
+                                                       day VARCHAR(10) NOT NULL,
+                                                       open_time VARCHAR(255),
+                                                       close_time VARCHAR(255),
+                                                       FOREIGN KEY (gym_department_id) REFERENCES gym_department(gym_department_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS gym_department_features (
+                                                       feature_id INT AUTO_INCREMENT PRIMARY KEY,
+                                                       gym_department_id INT NOT NULL,
+                                                       feature_icon varchar(150),
+                                                       feature_name VARCHAR(50),
+                                                       isSelected bit,
+                                                       FOREIGN KEY (gym_department_id) REFERENCES gym_department(gym_department_id)
+);
 
 -- table name mst_kbn to store type,status of all tables
 CREATE TABLE IF NOT EXISTS mst_kbn (
                                        mst_kbn_id    INT AUTO_INCREMENT PRIMARY KEY,
                                        mst_kbn_name  VARCHAR(50) NOT NULL,
-    mst_kbn_key   INT NOT NULL,
-    mst_kbn_value VARCHAR(50) NOT NULL
-    );
+                                       mst_kbn_key   INT NOT NULL,
+                                       mst_kbn_value VARCHAR(50) NOT NULL
+);
 
 -- Gym Plan table to store gym plan information
 CREATE TABLE IF NOT EXISTS gym_plan (
                                         plan_id        INT AUTO_INCREMENT PRIMARY KEY,
-                                        gym_deparment_id         INT NOT NULL,
+                                        gym_department_id         INT NOT NULL,
+                                        user_id        INT NOT NULL,
                                         gym_plan_key   INT NOT NULL,
                                         gym_plan_status_key    INT NOT NULL,
+                                        gym_plan_type_key  INT NOT NULL,
                                         name           VARCHAR(255) NOT NULL,
-    description    VARCHAR(255) NOT NULL,
-    price          DECIMAL(10, 2) ,
-    price_per_hours         DECIMAL(10, 2) ,
-    plan_sold      INT NOT NULL,
-    duration       INT ,
-    plan_before_active_validity       INT NOT NULL,
-    plan_after_active_validity        INT NOT NULL,
-    image_url      VARCHAR(255),
-    FOREIGN KEY (gym_deparment_id) REFERENCES gym_department(gym_department_id)
-    );
+                                        description    text,
+                                        price          DECIMAL(10, 2) ,
+                                        price_per_hours         DECIMAL(10, 2) ,
+                                        plan_sold      INT NOT NULL,
+                                        duration       INT ,
+                                        plan_before_active_validity       INT NOT NULL,
+                                        plan_after_active_validity        INT NOT NULL,
+                                        FOREIGN KEY (gym_department_id) REFERENCES gym_department(gym_department_id),
+                                        foreign key (user_id)references user(user_id)
+);
 
 -- Shift table to store shift information
 CREATE TABLE IF NOT EXISTS shift (
@@ -112,7 +144,7 @@ CREATE TABLE IF NOT EXISTS shift (
                                      start_time   TIME NOT NULL,
                                      end_time     TIME NOT NULL,
                                      FOREIGN KEY (gym_department_id) REFERENCES gym_department(gym_department_id)
-    );
+);
 
 -- Gymer Booking table to store user booking information
 CREATE TABLE IF NOT EXISTS gymer_booking (
@@ -124,9 +156,9 @@ CREATE TABLE IF NOT EXISTS gymer_booking (
                                              booking_time     DATETIME NOT NULL,
                                              booking_status   INT NOT NULL,
                                              FOREIGN KEY (user_id) REFERENCES `user`(user_id),
-    FOREIGN KEY (gym_department_id) REFERENCES gym_department(gym_department_id),
-    FOREIGN KEY (shift_id) REFERENCES shift(shift_id)
-    );
+                                             FOREIGN KEY (gym_department_id) REFERENCES gym_department(gym_department_id),
+                                             FOREIGN KEY (shift_id) REFERENCES shift(shift_id)
+);
 
 
 -- Transfer table to do the function related to transferring credit
@@ -135,19 +167,19 @@ CREATE TABLE IF NOT EXISTS transfer (
                                         sender_id      INT NOT NULL,
                                         receiver_id    INT NOT NULL,
                                         amount         DECIMAL(10, 2) NOT NULL,
-    transfer_date  DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (sender_id) REFERENCES `user`(user_id),
-    FOREIGN KEY (receiver_id) REFERENCES `user`(user_id)
-    );
+                                        transfer_date  DATETIME DEFAULT CURRENT_TIMESTAMP,
+                                        FOREIGN KEY (sender_id) REFERENCES `user`(user_id),
+                                        FOREIGN KEY (receiver_id) REFERENCES `user`(user_id)
+);
 
 -- Transaction table to do the function with transactions between credit and real money
 CREATE TABLE IF NOT EXISTS `transaction` (
                                              transaction_id   INT AUTO_INCREMENT PRIMARY KEY,
                                              wallet_id        INT NOT NULL,
                                              amount           DECIMAL(10, 2) NOT NULL,
-    transaction_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (wallet_id) REFERENCES wallet(wallet_id)
-    );
+                                             transaction_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+                                             FOREIGN KEY (wallet_id) REFERENCES wallet(wallet_id)
+);
 
 -- Order table to store order information
 CREATE TABLE IF NOT EXISTS `order` (
@@ -157,25 +189,25 @@ CREATE TABLE IF NOT EXISTS `order` (
                                        order_create_time      DATETIME NOT NULL,
                                        order_status_key    INT NOT NULL,
                                        order_money     DECIMAL(10, 2) NOT NULL,
-    discount              INT NOT NULL,
-    order_total_money     DECIMAL(10, 2) NOT NULL,
-    order_note                 VARCHAR(500),
-    FOREIGN KEY (user_id) REFERENCES `user`(user_id)
-    );
+                                       discount              INT NOT NULL,
+                                       order_total_money     DECIMAL(10, 2) NOT NULL,
+                                       order_note                 VARCHAR(500),
+                                       FOREIGN KEY (user_id) REFERENCES `user`(user_id)
+);
 
 -- Order Plan Detail table to store order plan details
 CREATE TABLE IF NOT EXISTS order_plan_detail (
                                                  order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
                                                  order_id        INT NOT NULL,
                                                  name              VARCHAR(255) NOT NULL,
-    quantity        INT NOT NULL,
-    price_per_hours         DECIMAL(10, 2) NOT NULL,
-    price           DECIMAL(10, 2) NOT NULL,
-    duration       INT NOT NULL,
-    plan_before_active_validity       INT NOT NULL,
-    plan_after_active_validity        INT NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES `order`(order_id)
-    );
+                                                 quantity        INT NOT NULL,
+                                                 price_per_hours         DECIMAL(10, 2) NOT NULL,
+                                                 price           DECIMAL(10, 2) NOT NULL,
+                                                 duration       INT NOT NULL,
+                                                 plan_before_active_validity       INT NOT NULL,
+                                                 plan_after_active_validity        INT NOT NULL,
+                                                 FOREIGN KEY (order_id) REFERENCES `order`(order_id)
+);
 
 -- User Inventory table to store user inventory information
 CREATE TABLE IF NOT EXISTS item_inventory (
@@ -186,8 +218,8 @@ CREATE TABLE IF NOT EXISTS item_inventory (
                                               item_status_key INT NOT NULL,
                                               plan_expired_time       DATETIME NOT NULL,
                                               FOREIGN KEY (user_id) REFERENCES `user`(user_id),
-    FOREIGN KEY (plan_id) REFERENCES gym_plan(plan_id)
-    );
+                                              FOREIGN KEY (plan_id) REFERENCES gym_plan(plan_id)
+);
 
 -- User Feedback table to store user feedback information
 CREATE TABLE IF NOT EXISTS user_feedback (
@@ -199,8 +231,8 @@ CREATE TABLE IF NOT EXISTS user_feedback (
                                              feedback_time  DATETIME NOT NULL,
                                              feedback_status INT NOT NULL,
                                              FOREIGN KEY (user_id) REFERENCES `user`(user_id),
-    FOREIGN KEY (department_id) REFERENCES gym_department(gym_department_id)
-    );
+                                             FOREIGN KEY (department_id) REFERENCES gym_department(gym_department_id)
+);
 
 DELIMITER $$
 CREATE TRIGGER update_gym_rating_avg
@@ -209,9 +241,9 @@ CREATE TRIGGER update_gym_rating_avg
 BEGIN
 
     DECLARE dept_id INT;
-DECLARE total, count INT;
+    DECLARE total, count INT;
 
-SET dept_id = NEW.department_id;
+    SET dept_id = NEW.department_id;
 
     SELECT IFNULL(SUM(rating),0), COUNT(*) INTO total, count
     FROM user_feedback
--- SQL/dml.sql
+++ SQL/dml.sql
@@ -33,9 +33,13 @@ INSERT INTO mst_kbn (mst_kbn_name, mst_kbn_key, mst_kbn_value) VALUES ('DEPARTME
 INSERT INTO mst_kbn (mst_kbn_name, mst_kbn_key, mst_kbn_value) VALUES ('DEPARTMENT_STATUS', 2, 'Không Hoạt Động');
 
 -- Create Department
-INSERT INTO gym_department
+INSERT INTO gym_department (gym_department_status_key, user_id, name, address, contact_number, logo_url, wallpaper_url, description, latitude, longitude, rating, capacity, area)
+VALUES (1, 1, 'Gym A', '123 Main St', '555-123-4567', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFg0vALj7wnJ-W6SbdUr7vhA278f3BPsMnQQ&usqp=CAU', 'https://example.com/wallpaper.png', 'Không chỉ là 1 phòng tập gym - thể hình đơn lẻ, Kickfit Sports là một trung tâm thể dụng thể thao với quy mô lớn và hiện đại hàng đầu Hà Nội. Các trang thiết bị luyện tập đều được nhập khẩu từ các thương hiệu lớn tại Châu Âu và Mỹ đem đến trải nghiệm tập luyện tốt nhất. Phòng tập được tích hợp đầy đủ bể bơi 4 mùa, phòng xông hơi, spa, sân tập ngoài trời và trên cao thoáng mát, tủ đồ, khu vực nghỉ ngơi... Ngoài tập gym bạn còn có thể trải nghiệm nhiều loại hình thể thao khác như: Boxing, Muay Thá', 37.12345678, -122.12345678, 0, 100, 200.50),
+       (1, 2, 'Gym B', '456 Elm St', '555-987-6543', 'https://example.com/logo.png', 'https://example.com/wallpaper.png', 'This is Gym B', 37.98765432, -122.98765432, 0, 150, 300.75);
+
+/** INSERT INTO gym_department
 (gym_department_id, gym_department_status_key, user_id, name, address, contact_number,
- logo_url, opening_hours, image_url, description, latitude, longitude)
+ logo_url, opening_hours, wallpaper_url, description, latitude, longitude,capacity,area)
 VALUES
     (1, 1, 2, 'Super Gym Hoa Lac', '123 Main St', '555-123-1234',
      'logo1.png', '8am-8pm', 'img1.jpg', 'Gym description', 20.994853642138313, 105.52473891128982),
@@ -43,6 +47,8 @@ VALUES
      'logo2.png', '9am-9pm', 'img2.jpg', 'Gym description', 20.984756591708862, 105.5304037366675),
     (3, 1, 2, 'FitWay Kickboxing', '125 Main St', '555-123-1236',
      'logo3.png', '10am-10pm', 'img3.jpg', 'Gym description', 21.013202882232747, 105.5189882552246);
+**/
+
 
 /********** Department Feedback Creation ***********/
 -- 2 records with department_id = 2
@@ -63,3 +69,50 @@ INSERT INTO user_feedback
 VALUES
     (5, 1, 3, 5, 'Loved the environment', NOW(), 1),
     (6, 2, 3, 4, 'Instructors were knowledgeable', NOW(), 1);
+INSERT INTO gym_department_albums (gym_department_id, photo_url, description)
+VALUES(1, 'https://i.pinimg.com/236x/6c/c4/49/6cc4498dfac9d232b9c49f46d1948f8b.jpg', 'Album 1'),
+      (1, 'https://i.pinimg.com/236x/62/ce/d1/62ced13c5fd204f575f47d4b026243dd.jpg', 'Album 2');
+
+
+INSERT INTO gym_department_schedule (gym_department_id, day, open_time, close_time)
+VALUES (1, 'Monday', '8:00 AM', '10:00 PM'),
+       (2, 'Tuesday', '9:00 AM', '11:00 PM');
+
+
+
+INSERT INTO mst_kbn (mst_kbn_name, mst_kbn_key, mst_kbn_value)
+VALUES ('Gym Plan Type', 1, 'Gói theo giờ'),
+       ('Gym Plan Type', 2, 'Gói không theo giờ'),
+       ('Gym Plan Status', 1, 'Chưa kích hoạt'),
+       ('Gym Plan Status', 2, 'Đang sử dụng'),
+       ('Gym Plan Status', 3, 'Đã sử dụng'),
+       ('Gym Plan Status', 4, 'Quá hạn');
+
+INSERT INTO gym_plan (gym_department_id, user_id, gym_plan_key, gym_plan_status_key, gym_plan_type_key, name, description, price, price_per_hours, plan_sold, duration, plan_before_active_validity, plan_after_active_validity)
+VALUES (1, 1, 1, 1, 1, 'Gói giờ', 'Đây là một trong các hạng thẻ siêu tiết kiệm tại phòng gym của chúng tôi. Nếu bạn là người không có nhu cầu di chuyển nhiều và mong muốn được tập luyện ở bất kỳ khung giờ nào thì gói tập này sẽ là một lựa chọn thích hợp. ', 100.00, 10.00, 20, 3, 7, 14),
+       (1, 1, 2, 2, 2, 'Gói 1 tháng', 'Đây là một trong những loại thẻ tập chính tại Gym Hòa Lạc. Khi tham gia gói tập này bạn sẽ có rất nhiều đặc quyền như được tập ở tất cả CLB cũng như phòng tập trên toàn bộ hệ thống theo từng khu vực Bắc, Trung hoặc Nam ', 150.00, 15.00, 30, 5, 10, 20);
+
+-- insert data to features
+INSERT INTO gym_department_features (gym_department_id, feature_icon, feature_name, isSelected)
+VALUES (1, 'icon1.png', 'Bể bơi', 1);
+
+INSERT INTO gym_department_features (gym_department_id, feature_icon, feature_name, isSelected)
+VALUES (1, 'icon2.png', 'Massage', 0);
+
+INSERT INTO gym_department_features (gym_department_id, feature_icon, feature_name, isSelected)
+VALUES (1, 'icon3.png', 'Xông hơi', 1);
+
+INSERT INTO gym_department_features (gym_department_id, feature_icon, feature_name, isSelected)
+VALUES (1, 'icon4.png', 'Phòng tắm', 1);
+
+INSERT INTO gym_department_features (gym_department_id, feature_icon, feature_name, isSelected)
+VALUES (1, 'icon5.png', 'Máy lạnh', 0);
+
+INSERT INTO gym_department_features (gym_department_id, feature_icon, feature_name, isSelected)
+VALUES (1, 'icon6.png', 'Boxing', 0);
+
+INSERT INTO gym_department_features (gym_department_id, feature_icon, feature_name, isSelected)
+VALUES (1, 'icon7.png', 'Quầy bar', 1);
+
+INSERT INTO gym_department_features (gym_department_id, feature_icon, feature_name, isSelected)
+VALUES (1, 'icon8.png', 'Máy đo chỉ số cơ thể', 1);
