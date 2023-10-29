74a79f541d36bb2d7a3be49f4197c8f3ce08bc55
tuanld3110 <ledinhtuana1@gmail.com>
2023-10-29 17:57:55 +0700

    TuanLD: update controller : add list brand

--- fitpass-web/src/main/java/com/ks/fitpass/web/controller/UserController.java
+++ fitpass-web/src/main/java/com/ks/fitpass/web/controller/UserController.java
@@ -3,6 +3,9 @@ package com.ks.fitpass.web.controller;
 import com.ks.fitpass.core.repository.UserRepository;
 import com.ks.fitpass.department.dto.DepartmentDTO;
 import com.ks.fitpass.department.service.DepartmentService;
+import com.ks.fitpass.brand.service.*;
+import com.ks.fitpass.brand.entity.*;
+
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -19,18 +22,20 @@ import java.util.List;
 public class UserController {
     private final UserRepository userRepository;
     private final DepartmentService departmentService;
+    private final BrandService brandService;
+
 
     @GetMapping("/user/homepage")
     public String userHomepage(Principal principal, HttpSession session, Model model) {
         com.ks.fitpass.core.entity.User user = userRepository.findByAccount(principal.getName());
         List<DepartmentDTO> departmentDTOList = departmentService.getAllDepartmentTopRatingForHome(1, 5);
+        List<Brand> brandList = brandService.getAllByStatus(1);
         model.addAttribute("departments", departmentDTOList);
-
+        model.addAttribute("brands", brandList);
         session.setAttribute("userInfo", user);
-        return "list-of-gym";
+        return "homepage-user";
+
     }
-
-
         @PostMapping("/user/homepage")
         public String getNearbyGyms ( @RequestParam("latitude") double latitude,
         @RequestParam("longitude") double longitude,
