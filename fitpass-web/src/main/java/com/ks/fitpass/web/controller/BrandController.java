febe88b49a2eb5076b4219682c66dbb3ffb58cd2
ming307- <94124200+Ming307mm@users.noreply.github.com>
2023-10-29 22:49:05 +0700

    Mi + fix Gym brand details

--- fitpass-web/src/main/java/com/ks/fitpass/web/controller/BrandController.java
+++ fitpass-web/src/main/java/com/ks/fitpass/web/controller/BrandController.java
@@ -3,6 +3,8 @@ package com.ks.fitpass.web.controller;
 import com.ks.fitpass.brand.entity.*;
 import com.ks.fitpass.brand.service.*;
 import com.ks.fitpass.department.dto.DepartmentDTO;
+import com.ks.fitpass.department.entity.DepartmentFeature;
+import com.ks.fitpass.department.service.DepartmentFeatureService;
 import com.ks.fitpass.department.service.DepartmentService;
 import com.ks.fitpass.web.enums.PageEnum;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -21,15 +24,17 @@ public class BrandController {
     private final BrandService brandService;
     private final BrandAmenitieService brandAmenitieService;
     private  final BrandAlbumsService brandAlbumsService;
-
     private final DepartmentService departmentService;
+    private final DepartmentFeatureService departmentFeatureService;
+
 
     @Autowired
-    public BrandController(BrandService brandService, BrandAmenitieService brandAmenitieService, BrandAlbumsService brandAlbumsService,DepartmentService departmentService) {
+    public BrandController(BrandService brandService, BrandAmenitieService brandAmenitieService, BrandAlbumsService brandAlbumsService, DepartmentService departmentService, DepartmentFeatureService departmentFeatureService) {
         this.brandService = brandService;
         this.brandAmenitieService = brandAmenitieService;
         this.brandAlbumsService = brandAlbumsService;
         this.departmentService = departmentService;
+        this.departmentFeatureService = departmentFeatureService;
     }
 
     @GetMapping("/brand-detail/{brand_id}")
@@ -47,6 +52,17 @@ public class BrandController {
 
             List<DepartmentDTO> departmentDTOList = departmentService.getDepartmentByBrandID(brandId);
             model.addAttribute("departments", departmentDTOList);
+
+            List<DepartmentFeature> departmentFeatures = new ArrayList<>();
+
+            // Loop through departmentDTOList to get departmentId values and fetch department features
+            for (DepartmentDTO departmentDTO : departmentDTOList) {
+                int departmentId = departmentDTO.getDepartmentId();
+                List<DepartmentFeature> features = departmentFeatureService.getDepartmentFeatures(departmentId);
+                departmentFeatures.addAll(features);
+            }
+
+            model.addAttribute("departmentFeatures", departmentFeatures);
 
             return "gym-brand-details";
         } catch (EmptyResultDataAccessException e) {
