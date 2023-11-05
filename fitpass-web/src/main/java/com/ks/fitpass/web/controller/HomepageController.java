6c470c5855d3f61991dce93137bf8c393bdef55f
trungnv79 <trung14112002wweliker@gmail.com>
2023-11-06 00:57:23 +0700

    TrungNV: modify change radius in km to 10

--- fitpass-web/src/main/java/com/ks/fitpass/web/controller/HomepageController.java
+++ fitpass-web/src/main/java/com/ks/fitpass/web/controller/HomepageController.java
@@ -61,7 +61,7 @@ public class HomepageController {
     public String getNearByDepartmentList(@RequestParam("userLatitude") double userLatitude,
                                           @RequestParam("userLongitude") double userLongitude, Model model) {
         Map<DepartmentDTO, Double> departmentDistanceMap = departmentService.getAllDepartmentByNearbyLocation(
-                1, 5, userLatitude, userLongitude, 5);
+                1, 5, userLatitude, userLongitude, 10);
         model.addAttribute("departmentDistanceMap",departmentDistanceMap);
         return "homepage/fragments/list-gym-fr";
     }
