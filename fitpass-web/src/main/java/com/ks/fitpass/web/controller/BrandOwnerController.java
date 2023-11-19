64153170741073e452f3cd1276304c503ad27676
tuanld3110 <ledinhtuana1@gmail.com>
2023-11-19 12:55:58 +0700

    modify brand owner controller modify department details get mapping

--- fitpass-web/src/main/java/com/ks/fitpass/web/controller/BrandOwnerController.java
+++ fitpass-web/src/main/java/com/ks/fitpass/web/controller/BrandOwnerController.java
@@ -5,8 +5,17 @@ import com.ks.fitpass.brand.entity.Brand;
 import com.ks.fitpass.brand.service.BrandService;
 import com.ks.fitpass.core.entity.User;
 import com.ks.fitpass.core.repository.UserRepository;
+import com.ks.fitpass.department.dto.DepartmentAmenitie;
+import com.ks.fitpass.department.dto.DepartmentDTO;
 import com.ks.fitpass.department.dto.DepartmentListByBrandDTO;
-import com.ks.fitpass.department.service.DepartmentService;
+import com.ks.fitpass.department.dto.GymPlanDto;
+import com.ks.fitpass.department.entity.Department;
+import com.ks.fitpass.department.entity.DepartmentAlbums;
+import com.ks.fitpass.department.entity.DepartmentFeature;
+import com.ks.fitpass.department.entity.DepartmentSchedule;
+import com.ks.fitpass.department.service.*;
+import com.ks.fitpass.gymplan.service.GymPlanService;
+import com.ks.fitpass.web.enums.PageEnum;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +33,11 @@ public class BrandOwnerController {
     private final UserRepository userRepository;
     private final BrandService brandService;
     private final DepartmentService departmentService;
+    private final GymPlanService gymPlanService;
+    private final DepartmentScheduleService departmentScheduleService;
+    private final DepartmentAlbumsService departmentAlbumsService;
+    private final DepartmentAmenitieService departmentAmenitieService;
+    private  final DepartmentFeatureService departmentFeatureService;
 
     //Index (Statistic Dashboard)
     @GetMapping("/index")
@@ -69,5 +83,38 @@ public class BrandOwnerController {
         model.addAttribute("brandId", brandId);
         model.addAttribute("departmentList", departmentDTOList);
         return "brand-owner/gym-brand-department-list";
+    }
+
+    @GetMapping("/department/details")
+    public String getDepartmentDetails(@RequestParam("id") int departmentId, Model model) {
+        Department department = departmentService.getOne(departmentId);
+        model.addAttribute("department", department);
+        model.addAttribute("page", PageEnum.XXX_FIRST_PAGE.getCode());
+
+        // Get list of gym plans for the department
+        List<GymPlanDto> gymPlans = gymPlanService.getGymPlanDetailsByDepartmentId(departmentId);
+        model.addAttribute("gymPlans", gymPlans);
+
+        // Get department schedule
+        List<DepartmentSchedule> departmentSchedules = departmentScheduleService.getAllByDepartmentID(departmentId);
+        model.addAttribute("departmentSchedules", departmentSchedules);
+
+        // Get Department Album
+        List<DepartmentAlbums> departmentAlbums = departmentAlbumsService.getAllByDepartmentID(departmentId);
+        model.addAttribute("departmentAlbums", departmentAlbums);
+
+        // Calculate the rating statistics
+        DepartmentDTO departmentDTO = departmentService.filterDepartmentFeedbacks(departmentId);
+        model.addAttribute("departmentFeedbacks", departmentDTO);
+
+        List<DepartmentAmenitie> departmentAmenitieList = departmentAmenitieService.getAllAmenitieOfDepartment(departmentId);
+        model.addAttribute("departmentAmenitieList", departmentAmenitieList);
+
+        // Get department features
+        List<DepartmentFeature> departmentFeatures = departmentFeatureService.getDepartmentFeatures(departmentId);
+        model.addAttribute("departmentFeatures", departmentFeatures);
+
+        model.addAttribute("departmentId", departmentId);
+        return "brand-owner/gym-brand-department-detail";
     }
 }
