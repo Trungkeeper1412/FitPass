5f69eb9e21336d3587f6a347991dbd66ecbeafb1
MSI\Baoli <linhnvbhe153459@fpt.edu.vn>
2023-11-20 00:39:21 +0700

    LinhNVB: update fit with backend validate add, update plan + validate add gym owner

--- fitpass-web/src/main/resources/static/employee-assets/js/validation/brand-owner-validation.js
+++ fitpass-web/src/main/resources/static/employee-assets/js/validation/brand-owner-validation.js
@@ -1,12 +1,27 @@
+$.validator.addMethod("minAge", function (value, element) {
+    var dob = new Date(value);
+    var today = new Date();
+    var age = today.getFullYear() - dob.getFullYear();
+
+    var monthDiff = today.getMonth() - dob.getMonth();
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < dob.getDate())) {
+        age--;
+    }
+    return age >= 18;
+}, "Bạn phải đủ 18 tuổi trở lên.");
+
 $(document).ready(function () {
     $("#formSubmit").validate({
         rules: {
+            // Validate Brand Name
             brandName: {
                 required: true,
                 minlength: 3,
                 maxlength: 32,
                 pattern: /^[a-zA-Z0-9\u00C0-\u1EF9 ]*$/,
             },
+
+            // Validate Plan
             gymPlanName: {
                 required: true,
                 minlength: 2,
@@ -31,21 +46,68 @@ $(document).ready(function () {
             description: {
                 required: true,
                 minlength: 2,
-                maxlength: 200,
-                pattern: /^[a-zA-Z0-9\u00C0-\u1EF9 ]*$/,
+                maxlength: 250,
             },
-            packageDuration: {
+            duration: {
                 required: true,
                 number: true,
                 min: 1,
             },
-            pricePlan: {
+            price: {
                 required: true,
                 number: true,
                 min: 0.01,
             },
+            statusActive: {
+                required: true,
+            },
+
+            // Validate Owner
+            'department-img': {
+                required: true
+            },
+            firstName: {
+                required: true,
+                maxlength: 25,
+                pattern: /^[a-zA-Z0-9\u00C0-\u1EF9 ]*$/,
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            address: {
+                required: true
+            },
+            dateOfBirth: {
+                required: true,
+                date: true,
+                minAge: true,
+            },
+            gender: {
+                required: true
+            },
+            lastName: {
+                required: true,
+                maxlength: 25,
+                pattern: /^[a-zA-Z0-9\u00C0-\u1EF9 ]*$/,
+            },
+            username: {
+                required: true,
+                minlength: 6,
+                maxlength: 50,
+                pattern: /^[a-zA-Z0-9]+$/,
+            },
+            phone: {
+                required: true,
+                digits: true
+            },
+            idCard: {
+                required: true,
+                digits: true
+            },
         },
         messages: {
+            // Validate Plan
             brandName: {
                 required: "Vui lòng nhập tên cơ sở",
                 minlength: "Tên cơ sở phải có ít nhất 3 kí tự",
@@ -76,20 +138,73 @@ $(document).ready(function () {
             description: {
                 required: "Vui lòng nhập nhập mô tả gói tập",
                 minlength: "Mô tả gói tập phải có ít nhất 2 kí tự",
-                maxlength: "Mô tả gói tập không được vượt quá 200 kí tự",
-                pattern: "Mô tả gói tập không được chứa kí tự đặc biệt",
+                maxlength: "Mô tả gói tập không được vượt quá 250 kí tự",
             },
-            packageDuration: {
+            duration: {
                 required: "Vui lòng nhập nhập số ngày sử dụng gói tập",
                 number: "Vui lòng nhập số hợp lệ",
                 min: "Số ngày ít nhất phải bằng 1"
             },
-            pricePlan: {
+            price: {
                 required: "Vui lòng nhập số credits",
                 number: "Vui lòng nhập số hợp lệ",
                 min: "Số credits phải lớn hơn 0",
             },
+            statusActive: {
+                required: "Vui lòng chọn trạng thái"
+            },
+
+            // Validate Owner
+            'department-img': {
+                required: "Vui lòng chọn ảnh"
+            },
+            firstName: {
+                required: "Vui lòng nhập họ của bạn",
+                maxlength: "Họ của bạn không được vượt quá 25 kí tự",
+                pattern: "Họ của bạn không được chứa kí tự đặc biệt",
+            },
+            email: {
+                required: "Vui lòng nhập email",
+                email: "Vui lòng nhập địa chỉ email hợp lệ"
+            },
+            address: {
+                required: "Vui lòng nhập địa chỉ"
+            },
+            dateOfBirth: {
+                required: "Vui lòng nhập ngày sinh",
+                date: "Vui lòng nhập ngày sinh hợp lệ",
+                minAge: "Bạn phải đủ 18 tuổi trở lên."
+            },
+            gender: {
+                required: "Vui lòng chọn giới tính"
+            },
+            lastName: {
+                required: "Vui lòng nhập tên của bạn",
+                maxlength: "Tên của bạn không được vượt quá 25 kí tự",
+                pattern: "Tên của bạn không được chứa kí tự đặc biệt",
+            },
+            username: {
+                required: "Vui lòng nhập tên đăng nhập",
+                minlength: "Tên đăng nhập phải có ít nhất 6 kí tự",
+                maxlength: "Tên đăng nhập không được vượt quá 50 kí tự",
+                pattern: "Tên đăng nhập không bao gồm khoảng trắng và ký tự đặc biệt",
+            },
+            phone: {
+                required: "Vui lòng nhập số điện thoại",
+                digits: "Vui lòng chỉ nhập số"
+            },
+            idCard: {
+                required: "Vui lòng nhập số căn cước",
+                digits: "Vui lòng chỉ nhập số"
+            },
         },
+        errorPlacement: function (error, element) {
+            if (element.attr("type") === "radio") {
+                error.insertAfter(element.closest('.display-status'));
+            } else {
+                error.insertAfter(element);
+            }
+        }
     });
 
     $("#createNewDepartment").click(function () {
