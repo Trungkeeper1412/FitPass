80f985fbe6b82e310d173e98b2a3800fb6382598
tuanld3110 <ledinhtuana1@gmail.com>
2023-11-19 12:46:07 +0700

    TuanLD: update brand owner controller modify profile mapping

--- fitpass-web/src/main/java/com/ks/fitpass/web/controller/BrandOwnerController.java
+++ fitpass-web/src/main/java/com/ks/fitpass/web/controller/BrandOwnerController.java
@@ -1,5 +1,8 @@
 package com.ks.fitpass.web.controller;
 
+import com.ks.fitpass.brand.entity.Brand;
+import com.ks.fitpass.brand.service.BrandService;
+import com.ks.fitpass.core.entity.User;
 import com.ks.fitpass.core.repository.UserRepository;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
@@ -14,12 +17,24 @@ import java.security.Principal;
 @RequiredArgsConstructor
 public class BrandOwnerController {
     private final UserRepository userRepository;
+    private final BrandService brandService;
 
     //Index (Statistic Dashboard)
     @GetMapping("/index")
-    public String getBOIndex(Principal principal, HttpSession session, Model model) {
+    public String getBOIndex(Principal principal, HttpSession session) {
         com.ks.fitpass.core.entity.User user = userRepository.findByAccount(principal.getName());
         session.setAttribute("userInfo", user);
         return "brand-owner/index";
+    }
+
+    //Brand Profile (Not a person)
+    @GetMapping("/profile")
+    public String getBrandProfile(HttpSession session, Model model) {
+        User user = (User) session.getAttribute("userInfo");
+        Brand brandDetails = brandService.getBrandDetail(user.getUserId());
+
+        model.addAttribute("time", System.currentTimeMillis());
+        model.addAttribute("brandDetails", brandDetails);
+        return "brand-owner/gym-brand-update-profile";
     }
 }
