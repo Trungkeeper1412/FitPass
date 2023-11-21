0d3fa55c9bf252afa34e14b1c32149863f233ca3
ming307- <94124200+Ming307mm@users.noreply.github.com>
2023-11-21 15:35:00 +0700

    Mi + update HomepageControllerTest

--- fitpass-web/src/test/java/HomepageControllerTest.java
+++ fitpass-web/src/test/java/HomepageControllerTest.java
@@ -174,7 +174,7 @@ public class HomepageControllerTest {
                 .thenThrow(new IllegalArgumentException());
 
         // Act
-        ResponseEntity<DepartmentHomePagePagnition> response = homepageController.getNearByDepartmentList(0.0, 0.0, 1, 0, "City", "sortPrice", "sortRating", "10");
+        ResponseEntity<DepartmentHomePagePagnition> response = homepageController.getNearByDepartmentList(0.0, 0.0, 1, 0, "City", "lowToHigh", "sortRating", "10");
 
         // Assert
         assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
