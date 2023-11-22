2c619c263b364868f9e408ac74cc438a6f8ece7d
tuanld3110 <ledinhtuana1@gmail.com>
2023-11-22 21:57:36 +0700

    TuanLD: update DDL table user_feedback gym_plan_id bỏ NOT NULL

--- SQL/ddl.sql
+++ SQL/ddl.sql
@@ -40,7 +40,6 @@ CREATE TABLE IF NOT EXISTS user_detail (
                                            gender        VARCHAR(10) NOT NULL,
                                            securityId   varchar(12) null,
                                            image_url TEXT NULL
-
 );
 -- User table to store user information
 CREATE TABLE IF NOT EXISTS `user` (
@@ -282,7 +281,7 @@ CREATE TABLE IF NOT EXISTS item_inventory (
 CREATE TABLE IF NOT EXISTS user_feedback (
                                              feedback_id    INT AUTO_INCREMENT PRIMARY KEY,
                                              user_id        INT NOT NULL,
-                                             gym_plan_id    INT NOT NULL,
+                                             gym_plan_id    INT,
                                              department_id  INT NOT NULL,
                                              rating         INT NOT NULL,
                                              comments       TEXT,
