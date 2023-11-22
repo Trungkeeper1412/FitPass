53612b4af7859af2f3c311cf45f11e8dcaa3b93c
tuanld3110 <ledinhtuana1@gmail.com>
2023-11-22 16:36:54 +0700

    TuanLD: update BrandCreateGymPlanFixedDTO gymPlanName

--- fitpass-gymplan/src/main/java/com/ks/fitpass/gymplan/dto/BrandCreateGymPlanFixedDTO.java
+++ fitpass-gymplan/src/main/java/com/ks/fitpass/gymplan/dto/BrandCreateGymPlanFixedDTO.java
@@ -7,8 +7,9 @@ import lombok.Data;
 public class BrandCreateGymPlanFixedDTO {
     private int brandId;
 
-    @NotEmpty(message = "Gym Plan Name cannot be blank")
-    @Size(max = 100, message = "Gym Plan Name must not exceed 100 characters")
+    @NotEmpty(message = "Vui lòng nhập tên gói tập !")
+    @Size(min = 2, max = 32, message = "Tên gói tập phải nằm trong khoảng 2 đến 32 kí tự !")
+    @Pattern(regexp = "^[a-zA-Z\\u00C0-\\u1EF9\\s]*$", message = "Tên gói tập không được chứa kí tự đặc biệt !")
     private String gymPlanName;
 
     @NotNull(message = "Price Per Hour cannot be null")
