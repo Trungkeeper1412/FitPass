5cd0a19c7cf22c2de265dc608c0b4df3eec9b59e
tuanld3110 <ledinhtuana1@gmail.com>
2023-11-22 15:09:00 +0700

    TuanLD: GymOwnerUpdateDTO gender, imageUrl

--- fitpass-brand/src/main/java/com/ks/fitpass/brand/dto/GymOwnerUpdateDTO.java
+++ fitpass-brand/src/main/java/com/ks/fitpass/brand/dto/GymOwnerUpdateDTO.java
@@ -43,10 +43,10 @@ public class GymOwnerUpdateDTO {
     @GymOwnerCreateDTO.ValidateIdCard(message = "Số căn cước công dân không hợp lệ !")
     private String idCard;
 
-    @NotEmpty(message = "Gender cannot be empty")
+    @NotEmpty(message = "Vui lòng chọn giới tính !")
     private String gender;
 
-    @NotEmpty(message = "Image url cannot be empty")
+    @NotEmpty(message = "Vui lòng chọn ảnh !")
     private String imageUrl;
 
 
