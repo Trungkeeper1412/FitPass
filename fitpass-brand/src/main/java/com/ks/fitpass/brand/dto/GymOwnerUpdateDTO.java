897dc43dcf991cf58d8142cb0d26a0f97ef6683d
tuanld3110 <ledinhtuana1@gmail.com>
2023-11-22 15:05:26 +0700

    TuanLD: GymOwnerUpdateDTO dateOfBirth

--- fitpass-brand/src/main/java/com/ks/fitpass/brand/dto/GymOwnerUpdateDTO.java
+++ fitpass-brand/src/main/java/com/ks/fitpass/brand/dto/GymOwnerUpdateDTO.java
@@ -33,8 +33,8 @@ public class GymOwnerUpdateDTO {
     @Size(max = 25, message = "Địa chỉ không được vượt quá 150 kí tự !")
     private String address;
 
-    @NotNull(message = "Date of Birth cannot be null")
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @NotNull(message = "Vui lòng nhập ngày sinh !")
+    @Past(message = "Vui lòng nhập ngày sinh hợp lệ !")
     private LocalDate dateOfBirth;
 
     //    @NotEmpty(message = "ID Card cannot be empty")
