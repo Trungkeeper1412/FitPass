ff0d567838e5d5d09936c9e103f697ec00f6e5ae
letrungduc2832 <letrungduc2832@gmail.com>
2023-11-24 02:17:12 +0700

    duclt_ update Department Scherdule Service

--- fitpass-department/src/main/java/com/ks/fitpass/department/service/DepartmentScheduleService.java
+++ fitpass-department/src/main/java/com/ks/fitpass/department/service/DepartmentScheduleService.java
@@ -4,8 +4,12 @@ import com.ks.fitpass.department.entity.DepartmentSchedule;
 import org.springframework.dao.DataAccessException;
 
 import java.util.List;
+import java.util.Map;
 
 public interface DepartmentScheduleService {
     List<DepartmentSchedule> getAllByDepartmentID(int departmentID) throws DataAccessException;
 
+    int[] addDepartmentSchedule(Map<String, String> dayToTimeMap, int departmentID);
+
+    int deleteAllDepartmentSchedule(int departmentId);
 }
