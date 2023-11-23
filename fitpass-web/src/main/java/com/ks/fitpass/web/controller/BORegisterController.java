e92198e57f7b8b717b2a0503fc3ac62ce93c721a
trungnv79 <trung14112002wweliker@gmail.com>
2023-11-23 21:35:21 +0700

    TrungNV: modify BORegisterController

--- fitpass-web/src/main/java/com/ks/fitpass/web/controller/BORegisterController.java
+++ fitpass-web/src/main/java/com/ks/fitpass/web/controller/BORegisterController.java
@@ -1,2 +1,14 @@
-package com.ks.fitpass.web.controller;public class BORegisterController {
+package com.ks.fitpass.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/become-a-partner")
+public class BORegisterController {
+    @GetMapping("/")
+    public String getBrandOwnerRegister(){
+        return "gym-brand-registration";
+    }
 }
