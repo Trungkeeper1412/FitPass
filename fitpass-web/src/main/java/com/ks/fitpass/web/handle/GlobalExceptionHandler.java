013faedc0b06101605a40d48dc625ff8a8a128f8
letrungduc2832 <letrungduc2832@gmail.com>
2023-11-23 13:55:01 +0700

    duclt_exception handler add

--- fitpass-web/src/main/java/com/ks/fitpass/web/handle/GlobalExceptionHandler.java
+++ fitpass-web/src/main/java/com/ks/fitpass/web/handle/GlobalExceptionHandler.java
@@ -1,53 +1,64 @@
 package com.ks.fitpass.web.handle;
 
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.support.MethodArgumentTypeMismatchException;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     // handleMethodArgumentTypeMismatch: triggers when a parameter's type does not match
-//    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-//    protected ResponseEntity<Object> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex, WebRequest request) {
-//        List<String> details = new ArrayList<>();
-//        details.add(ex.getMessage());
-//
-//        ResponseError err = ResponseError.builder()
-//                .timestamp(LocalDateTime.now())
-//                .status(HttpStatus.BAD_REQUEST)
-//                .message("Mismatch Type")
-//                .errors(details)
-//                .build();
-//        return ResponseEntityBuilder.build(err);
-//    }
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    protected ResponseEntity<Object> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex, WebRequest request) {
+        List<String> details = new ArrayList<>();
+        details.add(ex.getMessage());
+
+        ResponseError err = ResponseError.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.BAD_REQUEST)
+                .message("Mismatch Type")
+                .errors(details)
+                .build();
+        return ResponseEntityBuilder.build(err);
+    }
 
     // handleEmptyResultDataAccessException: triggers when a parameter's type does not match
-//    @ExceptionHandler(EmptyResultDataAccessException.class)
-//    protected ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex, WebRequest request) {
-//        List<String> details = new ArrayList<>();
-//        details.add(ex.getMessage());
-//
-//        ResponseError err = ResponseError.builder()
-//                .timestamp(LocalDateTime.now())
-//                .status(HttpStatus.NOT_FOUND)
-//                .message("Not Found")
-//                .errors(details)
-//                .build();
-//        return ResponseEntityBuilder.build(err);
-//    }
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    protected ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex, WebRequest request) {
+        List<String> details = new ArrayList<>();
+        details.add(ex.getMessage());
+
+        ResponseError err = ResponseError.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.NOT_FOUND)
+                .message("Not Found")
+                .errors(details)
+                .build();
+        return ResponseEntityBuilder.build(err);
+    }
 
     // handleNullPointerException
-//    @ExceptionHandler(NullPointerException.class)
-//    protected ResponseEntity<Object> handleNullPointerException(NullPointerException ex, WebRequest request, Model model) {
-//        List<String> details = new ArrayList<>();
-//        details.add(ex.getMessage());
-//
-//        ResponseError err = ResponseError.builder()
-//                .timestamp(LocalDateTime.now())
-//                .status(HttpStatus.BAD_REQUEST)
-//                .message("Null Pointer Exception")
-//                .errors(details)
-//                .build();
-//        return ResponseEntityBuilder.build(err);
-//    }
+    @ExceptionHandler(NullPointerException.class)
+    protected ResponseEntity<Object> handleNullPointerException(NullPointerException ex, WebRequest request, Model model) {
+        List<String> details = new ArrayList<>();
+        details.add(ex.getMessage());
+
+        ResponseError err = ResponseError.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.BAD_REQUEST)
+                .message("Null Pointer Exception")
+                .errors(details)
+                .build();
+        return ResponseEntityBuilder.build(err);
+    }
 }
