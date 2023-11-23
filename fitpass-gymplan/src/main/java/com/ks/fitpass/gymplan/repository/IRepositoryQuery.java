2b064d34630703a0ce288f2abe7bfdc56af574f2
letrungduc2832 <letrungduc2832@gmail.com>
2023-11-24 02:25:04 +0700

    duclt_update query

--- fitpass-gymplan/src/main/java/com/ks/fitpass/gymplan/repository/IRepositoryQuery.java
+++ fitpass-gymplan/src/main/java/com/ks/fitpass/gymplan/repository/IRepositoryQuery.java
@@ -41,7 +41,8 @@ public interface IRepositoryQuery {
                 	   price_per_hours,
                 	   plan_before_active_validity,
                 	   plan_after_active_validity,
-                	   gym_plan_status_key as status
+                	   gym_plan_status_key as status,
+                	   description
                 FROM gym_plan
                 where brand_id = ?
                 and gym_plan_type_key = 1
@@ -79,7 +80,8 @@ public interface IRepositoryQuery {
                 	   duration,
                 	   plan_before_active_validity,
                 	   plan_after_active_validity,
-                	   gym_plan_status_key as status
+                	   gym_plan_status_key as status,
+                	   description
                 FROM gym_plan
                 where brand_id = ?
                 and gym_plan_type_key = 2
@@ -109,5 +111,45 @@ public interface IRepositoryQuery {
                 UPDATE gym_plan
                 SET gym_plan_status_key= ?, name=?, description=?, price=?, plan_before_active_validity=?, plan_after_active_validity=?, duration = ?
                 WHERE plan_id= ?;
+            """;
+
+    String INSERT_GYM_PLAN_DEPARTMENT = """
+                INSERT INTO gym_department_plans
+                (gym_department_id, plan_id)
+                VALUES(?, ?);
+            """;
+
+    String GET_GYM_PLAN_DEPARTMENT_FIXED_BY_DEPARTMENT_ID = """
+                SELECT gp.plan_id ,
+                	   gp.name,
+                	   gp.price,
+                	   gp.duration,
+                	   gp.plan_before_active_validity,
+                	   gp.plan_after_active_validity,
+					   gp.description
+                FROM gym_plan gp
+                JOIN gym_department_plans gdp ON gp.plan_id = gdp.plan_id
+                JOIN gym_department gd ON gdp.gym_department_id = gd.gym_department_id
+                WHERE gdp.gym_department_id = ?
+                AND gp.gym_plan_type_key = 2;
+            """;
+
+    String GET_GYM_PLAN_DEPARTMENT_FLEX_BY_DEPARTMENT_ID = """
+                SELECT gp.plan_id ,
+                	   gp.name,
+                	   gp.price_per_hours,
+                	   gp.plan_before_active_validity,
+                	   gp.plan_after_active_validity,
+                       gp.description
+                FROM gym_plan gp
+                JOIN gym_department_plans gdp ON gp.plan_id = gdp.plan_id
+                JOIN gym_department gd ON gdp.gym_department_id = gd.gym_department_id
+                WHERE gdp.gym_department_id = ?
+                AND gp.gym_plan_type_key = 1;
+            """;
+
+    String DELETE_ALL_GYM_PLAN_BY_DEPARTMENT_ID = """
+                DELETE FROM gym_department_plans
+                WHERE gym_department_id = ?;
             """;
 }
