0c43986561b6e3a2d876abddd4ac4a1db12bc4fe
letrungduc2832 <letrungduc2832@gmail.com>
2023-11-24 18:26:04 +0700

    duclt_update departmentAlbum repo add delete album and create album methods

--- fitpass-department/src/main/java/com/ks/fitpass/department/repository/impl/DepartmentAlbumsRepositoryImpl.java
+++ fitpass-department/src/main/java/com/ks/fitpass/department/repository/impl/DepartmentAlbumsRepositoryImpl.java
@@ -6,9 +6,12 @@ import com.ks.fitpass.department.repository.DepartmentAlbumsRepository;
 import com.ks.fitpass.department.repository.IRepositoryQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 
@@ -23,6 +26,27 @@ public class DepartmentAlbumsRepositoryImpl implements DepartmentAlbumsRepositor
     @Override
     public List<DepartmentAlbums> getAllByDepartmentID(int departmentID) throws DataAccessException {
         return jdbcTemplate.query(GET_DEPARTMENT_ALBUMS_BY_ID_DEPARTMENT, new DepartmentAlbumsMapper(), departmentID);
+    }
+
+    @Override
+    public int deleteAllAlbumsByDepartmentID(int departmentID) throws DataAccessException{
+        return jdbcTemplate.update(DELETE_DEPARTMENT_ALBUMS_BY_ID_DEPARTMENT, departmentID);
+    }
+
+    @Override
+    public int[] addDepartmentAlbums(List<DepartmentAlbums> departmentAlbums) throws DataAccessException{
+        return jdbcTemplate.batchUpdate(INSERT_DEPARTMENT_ALBUM, new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                ps.setInt(1, departmentAlbums.get(i).getDepartmentId());
+                ps.setString(2, departmentAlbums.get(i).getPhotoUrl());
+            }
+
+            @Override
+            public int getBatchSize() {
+                return departmentAlbums.size();
+            }
+        });
     }
 }
 
