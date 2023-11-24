48002e69e6f338b0454616a039371fe85b2f71f9
tuanld3110 <ledinhtuana1@gmail.com>
2023-11-25 01:58:50 +0700

    TuanLD: update GymOwnerCreateDTO lastName

--- fitpass-brand/src/main/java/com/ks/fitpass/brand/dto/GymOwnerCreateDTO.java
+++ fitpass-brand/src/main/java/com/ks/fitpass/brand/dto/GymOwnerCreateDTO.java
@@ -26,7 +26,7 @@ public class GymOwnerCreateDTO {
     @Pattern(regexp = "^[a-zA-Z\\u00C0-\\u1EF9\\s]*$", message = "Họ của bạn không được chứa kí tự đặc biệt !")
     private String firstName;
 
-    @Size(max = 50, message = "Tên của bạn không được vượt quá 25 kí tự !")
+    @Size(max = 25, message = "Tên của bạn không được vượt quá 25 kí tự !")
     @NotEmpty(message = "Vui lòng nhập tên của bạn !")
     @Pattern(regexp = "^[a-zA-Z\\u00C0-\\u1EF9\\s]*$", message = "Tên của bạn không được chứa kí tự đặc biệt !")
     private String lastName;
