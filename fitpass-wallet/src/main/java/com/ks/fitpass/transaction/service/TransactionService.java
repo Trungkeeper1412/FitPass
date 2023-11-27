a11cc0d814cf72c84e3bf6a21b086b96fd46c2ec
letrungduc2832 <letrungduc2832@gmail.com>
2023-11-27 15:19:08 +0700

    duclt_update Transaction Service

--- fitpass-wallet/src/main/java/com/ks/fitpass/transaction/service/TransactionService.java
+++ fitpass-wallet/src/main/java/com/ks/fitpass/transaction/service/TransactionService.java
@@ -1,10 +1,13 @@
 package com.ks.fitpass.transaction.service;
 
 import com.ks.fitpass.transaction.dto.TransactionDTO;
+import com.ks.fitpass.transaction.dto.TransferCreditHistory;
 
 import java.util.List;
 
 public interface TransactionService {
     int insertTransaction(TransactionDTO transactionDTO);
     List<TransactionDTO> getListTransactionByUserId(int userId);
+
+    int insertTransferCreditHistory(TransferCreditHistory transferCreditHistory);
 }
