c7beced0211eaf8ac4826b766f77149af4fe7fa6
tuanld3110 <ledinhtuana1@gmail.com>
2023-11-27 17:31:18 +0700

    TuanLD: update file Phone NumberVali

--- fitpass-department/src/main/java/com/ks/fitpass/department/dto/PhoneNumberValidator.java
+++ fitpass-department/src/main/java/com/ks/fitpass/department/dto/PhoneNumberValidator.java
@@ -1,2 +1,25 @@
-package com.ks.fitpass.department.dto;public class PhoneNumberValidator {
+package com.ks.fitpass.department.dto;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class PhoneNumberValidator implements ConstraintValidator<UpdateGymOwnerDepartmentInfo.ValidatePhone, String> {
+
+    private String mobileRegex = "^(0[3|5|7|8|9])+([0-9]{8})$";
+    private String hotlineRegex = "^(1800|1900)\\d{6}$";
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+
+        if(value == null || value.isEmpty()) {
+            return false;
+        }
+
+        if(!value.matches(mobileRegex) && !value.matches(hotlineRegex)) {
+            return false;
+        }
+
+        return true;
+    }
+
+
 }
