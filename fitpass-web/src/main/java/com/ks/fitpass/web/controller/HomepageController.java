0c4f393dbeacb67446b645f25e2f10b3fc19b928
ming307- <94124200+Ming307mm@users.noreply.github.com>
2023-12-03 00:58:29 +0700

    My + add search result in Homepage

--- fitpass-web/src/main/java/com/ks/fitpass/web/controller/HomepageController.java
+++ fitpass-web/src/main/java/com/ks/fitpass/web/controller/HomepageController.java
@@ -137,4 +137,9 @@ public class HomepageController {
             throw new IllegalArgumentException("Invalid distance format.");
         }
     }
+
+    @GetMapping("/homepage/search")
+    public String getSearchResult(HttpSession session) {
+        return "search-gym-result";
+    }
 }
