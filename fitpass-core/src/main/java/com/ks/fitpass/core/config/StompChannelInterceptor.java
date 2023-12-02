73b3cb0271cbcaf34522476f41c75047a4189b80
trungnv79 <trung14112002wweliker@gmail.com>
2023-12-03 02:30:25 +0700

    TrungNV: add StompChannelInterceptor

---
+++ fitpass-core/src/main/java/com/ks/fitpass/core/config/StompChannelInterceptor.java
@@ -0,0 +1,31 @@
+package com.ks.fitpass.core.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StompChannelInterceptor implements ChannelInterceptor {
+    private static final Logger logger = LoggerFactory.getLogger(StompChannelInterceptor.class);
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+
+        if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+            Object raw = message.getHeaders().get(SimpMessageHeaderAccessor.NATIVE_HEADERS);
+            logger.error("The raw message header {}:", raw);
+
+        }
+
+        logger.error("The raw message {}:", message);
+        return message;
+    }
+}
