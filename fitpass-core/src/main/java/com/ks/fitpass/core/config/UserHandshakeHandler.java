2dcf5ddc479b3e17a4711ae26379ad2900bba339
trungnv79 <trung14112002wweliker@gmail.com>
2023-12-03 02:48:39 +0700

    TrungNV: modify UserHandshakeHandler return new User principal

--- fitpass-core/src/main/java/com/ks/fitpass/core/config/UserHandshakeHandler.java
+++ fitpass-core/src/main/java/com/ks/fitpass/core/config/UserHandshakeHandler.java
@@ -1,31 +1,27 @@
 package com.ks.fitpass.core.config;
-import com.ks.fitpass.core.entity.CustomUser;
-import com.sun.security.auth.UserPrincipal;
+
+import com.ks.fitpass.core.entity.User;
+import jakarta.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.server.ServerHttpRequest;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 import java.security.Principal;
 import java.util.Map;
 
+@Component
 public class UserHandshakeHandler extends DefaultHandshakeHandler {
-    private final Logger LOG = LoggerFactory.getLogger(UserHandshakeHandler.class);
 
     @Override
     protected Principal determineUser(ServerHttpRequest request, WebSocketHandler wsHandler, Map<String, Object> attributes) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.getPrincipal() instanceof CustomUser customUserDetails) {
-            String id = String.valueOf(customUserDetails.getId());
-            // ... other attributes
-            LOG.info("User with ID " + id + " opened the page");
-
-            return new UserPrincipal(id);
-        }
-        return null;
+        ServletServerHttpRequest servletRequest = (ServletServerHttpRequest) request;
+        HttpSession session = servletRequest.getServletRequest().getSession();
+        User user = (User) session.getAttribute("userInfo");
+        return () -> String.valueOf(user.getUserId());
     }
 }
 
