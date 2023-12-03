562acda65b785ccd61625bdfed11fa82c7ee62a3
tuanld3110 <ledinhtuana1@gmail.com>
2023-12-03 14:13:49 +0700

    TuanLD: update BORegisterController

--- fitpass-web/src/main/java/com/ks/fitpass/web/controller/BORegisterController.java
+++ fitpass-web/src/main/java/com/ks/fitpass/web/controller/BORegisterController.java
@@ -1,15 +1,46 @@
 package com.ks.fitpass.web.controller;
 
+import com.ks.fitpass.become_a_partner.dto.BecomePartnerForm;
+import com.ks.fitpass.become_a_partner.dto.BecomePartnerRequest;
+import com.ks.fitpass.become_a_partner.service.BecomePartnerService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Timestamp;
 
 @Controller
 @RequestMapping("/become-a-partner")
+@RequiredArgsConstructor
 public class BORegisterController {
+    private final BecomePartnerService becomePartnerService;
+
     @GetMapping("")
-    public String getBrandOwnerRegister(){
+    public String getBrandOwnerRegister(Model model){
+        model.addAttribute("becomePartnerForm", new BecomePartnerForm());
         return "gym-brand-registration";
+    }
+
+    @PostMapping("/submitForm")
+    public String postBrandOwnerRegister(@Valid  @ModelAttribute BecomePartnerForm becomePartnerForm,
+                                         BindingResult bindingResult){
+        if (bindingResult.hasErrors()) {
+            return "gym-brand-registration";
+        }
+        BecomePartnerRequest becomePartnerRequest = new BecomePartnerRequest();
+        becomePartnerRequest.setBrandName(becomePartnerForm.getBrandName());
+        becomePartnerRequest.setBrandOwnerName(becomePartnerForm.getBrandOwnerName());
+        becomePartnerRequest.setContactNumber(becomePartnerForm.getContactNumber());
+        becomePartnerRequest.setAddress(becomePartnerForm.getAddress());
+        becomePartnerRequest.setWebUrl(becomePartnerForm.getWebUrl());
+        becomePartnerRequest.setContactEmail(becomePartnerForm.getContactEmail());
+        becomePartnerRequest.setSendRequestTime(new Timestamp(System.currentTimeMillis()));
+        becomePartnerRequest.setStatus("Đang chờ xử lý");
+        int row = becomePartnerService.create(becomePartnerRequest);
+        return "redirect:/become-a-partner/successful";
     }
     @GetMapping("/successful")
     public String getRegistrationSuccessful() {
