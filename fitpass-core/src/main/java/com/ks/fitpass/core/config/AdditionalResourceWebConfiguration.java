305dd13b3b59a1425fc85f2ed2e26ad9f2015e75
trungnv79 <trung14112002wweliker@gmail.com>
2023-12-07 02:10:36 +0700

    TrungNV: Update resourceHandler Config

--- fitpass-core/src/main/java/com/ks/fitpass/core/config/AdditionalResourceWebConfiguration.java
+++ fitpass-core/src/main/java/com/ks/fitpass/core/config/AdditionalResourceWebConfiguration.java
@@ -13,21 +13,21 @@ import java.util.concurrent.TimeUnit;
 @Configuration
 public class AdditionalResourceWebConfiguration implements WebMvcConfigurer {
 
-    @Override
-    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-        Path path = Paths.get("./upload/img");
-        String upload = path.toFile().getAbsolutePath();
-        registry.addResourceHandler("/img/**").
-                addResourceLocations("file:/" + upload + "/").
-                setCacheControl(CacheControl.noCache());
-    }
-
 //    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        // Specify full path to images on Ubuntu file system
-//        String imagesPath = "/home/upload/img/";
-//        registry.addResourceHandler("/img/**")
-//                .addResourceLocations("file:" + imagesPath)
-//                .setCacheControl(CacheControl.maxAge(2, TimeUnit.HOURS));
+//    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+//        Path path = Paths.get("./upload/img");
+//        String upload = path.toFile().getAbsolutePath();
+//        registry.addResourceHandler("/img/**").
+//                addResourceLocations("file:/" + upload + "/").
+//                setCacheControl(CacheControl.noCache());
 //    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Specify full path to images on Ubuntu file system
+        String imagesPath = "/home/upload/img/";
+        registry.addResourceHandler("/img/**")
+                .addResourceLocations("file:" + imagesPath)
+                .setCacheControl(CacheControl.maxAge(2, TimeUnit.HOURS));
+    }
 }
