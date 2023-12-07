975f00273b5c1c2a9d80125eee23ad783211e7c0
trungnv79 <trung14112002wweliker@gmail.com>
2023-12-07 17:35:38 +0700

    TrungNV: Change method upload for brand images

--- fitpass-web/src/main/java/com/ks/fitpass/web/controller/FileUploadController.java
+++ fitpass-web/src/main/java/com/ks/fitpass/web/controller/FileUploadController.java
@@ -26,6 +26,31 @@ public class FileUploadController {
     //private static final String UPLOAD_DIRECTORY = "fitpass-web/src/main/resources/static/images/";
     private static final String UPLOAD_DIRECTORY = "upload/img/";
     private final Logger logger = LoggerFactory.getLogger(DepartmentController.class);
+
+    private String determineDirectoryBasedOnType(String type) {
+        switch (type) {
+            case "logo":
+                return "logos";
+            case "thumbnail":
+                return "thumbnails";
+            case "wallpaper":
+                return "wallpapers";
+            default:
+                return ""; // Invalid type
+        }
+    }
+    private String determineFileNameBasedOnType(String type) {
+        switch (type) {
+            case "logo":
+                return "logo";
+            case "thumbnail":
+                return "thumbnail";
+            case "wallpaper":
+                return "wallpaper";
+            default:
+                return ""; // Invalid type
+        }
+    }
     @PostMapping("/file")
     public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
         if (file.isEmpty()) {
@@ -47,67 +72,124 @@ public class FileUploadController {
         }
     }
 
-    @PostMapping("/{type}/{brandId}")
-    public ResponseEntity<String> uploadBrandImage(
-            @PathVariable String type,
-            @PathVariable int brandId,
-            @RequestParam("file") MultipartFile file) {
+//    @PostMapping("/{type}/{brandId}")
+//    public ResponseEntity<String> uploadBrandImage(
+//            @PathVariable String type,
+//            @PathVariable int brandId,
+//            @RequestParam("file") MultipartFile file) {
+//
+//        if (file.isEmpty()) {
+//            return ResponseEntity.badRequest().body("Please select a file to upload");
+//        }
+//
+//        try {
+//            String originalFileName = file.getOriginalFilename();
+//
+//            // Kiểm tra định dạng của tệp
+//            String fileExtension = getFileExtension(originalFileName);
+//            if (!isValidFileExtension(fileExtension)) {
+//                return ResponseEntity.badRequest().body("Invalid file format. Only PNG and JPEG files are allowed.");
+//            }
+//
+//            // Xác định thư mục dựa trên loại hình ảnh
+//            String directory = type.equals("logo")
+//                    ? "logos"
+//                    : type.equals("thumbnail")
+//                    ? "thumbnails"
+//                    : type.equals("wallpaper")
+//                    ? "wallpapers"
+//                    : "";
+//
+//            if (!directory.isEmpty()) {
+//                // Tạo thư mục nếu nó chưa tồn tại
+//                Path brandDirectoryPath = Paths.get(UPLOAD_DIRECTORY + "brands/", directory);
+//                if (!Files.exists(brandDirectoryPath)) {
+//                    Files.createDirectories(brandDirectoryPath);
+//                }
+//
+//                String fileName = type.equals("logo")
+//                        ? "logo"
+//                        : type.equals("thumbnail")
+//                        ? "thumbnail"
+//                        : "wallpaper";
+//
+//                // Xác định đường dẫn tới tệp đã tải lên
+//                Path filePath = brandDirectoryPath.resolve(brandId + "_" + fileName + "." + fileExtension);
+//
+//                // Sử dụng StandardCopyOption.REPLACE_EXISTING để ghi đè tệp nếu nó tồn tại
+//                Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+//
+//                // Trả về đường dẫn tới tệp đã tải lên
+//                String uploadedFilePath = "/img/brands/" + directory + "/" + brandId + "_" + fileName + "." + fileExtension;
+//
+//                return ResponseEntity.ok(uploadedFilePath);
+//            } else {
+//                return ResponseEntity.badRequest().body("Invalid image type");
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            return ResponseEntity.status(500).body("Failed to upload file");
+//        }
+//    }
+@PostMapping("/{type}/{brandId}")
+public ResponseEntity<String> uploadBrandImage(
+        @PathVariable String type,
+        @PathVariable int brandId,
+        @RequestParam("file") MultipartFile file) {
 
-        if (file.isEmpty()) {
-            return ResponseEntity.badRequest().body("Please select a file to upload");
-        }
+    logger.info("Received a request to upload an image for brandId: {} and image type: {}", brandId, type);
 
-        try {
-            String originalFileName = file.getOriginalFilename();
-
-            // Kiểm tra định dạng của tệp
-            String fileExtension = getFileExtension(originalFileName);
-            if (!isValidFileExtension(fileExtension)) {
-                return ResponseEntity.badRequest().body("Invalid file format. Only PNG and JPEG files are allowed.");
-            }
-
-            // Xác định thư mục dựa trên loại hình ảnh
-            String directory = type.equals("logo")
-                    ? "logos"
-                    : type.equals("thumbnail")
-                    ? "thumbnails"
-                    : type.equals("wallpaper")
-                    ? "wallpapers"
-                    : "";
-
-            if (!directory.isEmpty()) {
-                // Tạo thư mục nếu nó chưa tồn tại
-                Path brandDirectoryPath = Paths.get(UPLOAD_DIRECTORY + "brands/", directory);
-                if (!Files.exists(brandDirectoryPath)) {
-                    Files.createDirectories(brandDirectoryPath);
-                }
-
-                String fileName = type.equals("logo")
-                        ? "logo"
-                        : type.equals("thumbnail")
-                        ? "thumbnail"
-                        : type.equals("wallpaper")
-                        ? "wallpaper"
-                        : "";
-
-                // Xác định đường dẫn tới tệp đã tải lên
-                Path filePath = brandDirectoryPath.resolve(brandId + "_" + fileName + "." + fileExtension);
-
-                // Sử dụng StandardCopyOption.REPLACE_EXISTING để ghi đè tệp nếu nó tồn tại
-                Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
-
-                // Trả về đường dẫn tới tệp đã tải lên
-                String uploadedFilePath = "/img/brands/" + directory + "/" + brandId + "_" + fileName + "." + fileExtension;
-
-                return ResponseEntity.ok(uploadedFilePath);
-            } else {
-                return ResponseEntity.badRequest().body("Invalid image type");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            return ResponseEntity.status(500).body("Failed to upload file");
-        }
+    if (file.isEmpty()) {
+        logger.warn("File upload failed because the file was empty");
+        return ResponseEntity.badRequest().body("Please select a file to upload");
     }
+
+    try {
+        String originalFileName = file.getOriginalFilename();
+        logger.info("Original file name: {}", originalFileName);
+
+        // Kiểm tra định dạng của tệp
+        String fileExtension = getFileExtension(originalFileName);
+        if (!isValidFileExtension(fileExtension)) {
+            logger.warn("Invalid file format for file: {}", originalFileName);
+            return ResponseEntity.badRequest().body("Invalid file format. Only PNG and JPEG files are allowed.");
+        }
+
+        // Xác định thư mục dựa trên loại hình ảnh
+        String directory = determineDirectoryBasedOnType(type);
+        logger.info("Determined directory: {}", directory);
+
+        if (!directory.isEmpty()) {
+            // Tạo thư mục nếu nó chưa tồn tại
+            Path brandDirectoryPath = Paths.get(UPLOAD_DIRECTORY + "brands/", directory);
+            if (!Files.exists(brandDirectoryPath)) {
+                logger.info("Directory does not exist, creating: {}", brandDirectoryPath);
+                Files.createDirectories(brandDirectoryPath);
+            }
+
+            String fileName = determineFileNameBasedOnType(type);
+            // Xác định đường dẫn tới tệp đã tải lên
+            Path filePath = brandDirectoryPath.resolve(brandId + "_" + fileName + "." + fileExtension);
+            logger.info("Resolved file path: {}", filePath);
+
+            // Sử dụng StandardCopyOption.REPLACE_EXISTING để ghi đè tệp nếu nó tồn tại
+            Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+            logger.info("Successfully copied file to: {}", filePath);
+
+            // Trả về đường dẫn tới tệp đã tải lên
+            String uploadedFilePath = "/img/brands/" + directory + "/" + brandId + "_" + fileName + "." + fileExtension;
+            logger.info("Returning uploaded file path: {}", uploadedFilePath);
+
+            return ResponseEntity.ok(uploadedFilePath);
+        } else {
+            logger.warn("Invalid image type: {}", type);
+            return ResponseEntity.badRequest().body("Invalid image type");
+        }
+    } catch (IOException e) {
+        logger.error("Failed to upload file due to IOException", e);
+        return ResponseEntity.status(500).body("Failed to upload file");
+    }
+}
 
     @PostMapping("/department/{type}/{departmentId}")
     public ResponseEntity<String> uploadDepartmentImage(
@@ -232,7 +314,7 @@ public class FileUploadController {
             // Kiểm tra định dạng của tệp
             String fileExtension = getFileExtension(originalFileName);
             if (!isValidFileExtension(fileExtension)) {
-                return ResponseEntity.badRequest().body("Invalid file format. Only PNG and JPEG files are allowed.");
+                return ResponseEntity.badRequest().body("Invalid file format. Only JPG, PNG and JPEG files are allowed.");
             }
 
             // Tạo thư mục nếu nó chưa tồn tại
